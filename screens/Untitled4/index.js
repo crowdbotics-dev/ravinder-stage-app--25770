7a0b83dd447316937e89681356b2d7a4357427cd
Ravinder yadav <ravinder.y@crowdbotics.com>
2022-10-11 18:36:00 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled4/index.js
@@ -0,0 +1,35 @@
+import { TextInput } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled4 = () => {
+  return <View style={_styles.IZUDBLAt}>
+      <TextInput style={_styles.ZRHJZuXq}></TextInput><TextInput style={_styles.vktPLdjR}></TextInput></View>;
+};
+
+export default Untitled4;
+
+const _styles = StyleSheet.create({
+  IZUDBLAt: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  ZRHJZuXq: {
+    left: 83,
+    top: 144,
+    position: "absolute",
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 198,
+    height: 70
+  },
+  vktPLdjR: {
+    position: "absolute",
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  }
+});

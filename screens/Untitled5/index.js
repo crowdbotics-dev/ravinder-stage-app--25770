f45a8e34de4a101f82c8bbb47e3e305f2663b974
Ravinder yadav <ravinder.y@crowdbotics.com>
2022-10-25 12:15:14 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled5/index.js
@@ -0,0 +1,29 @@
+import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled5 = () => {
+  return <View style={_styles.DXxeQbwp}>
+      <Text style={_styles.JTfPLUxu}>Lorem ipsum…</Text></View>;
+};
+
+export default Untitled5;
+
+const _styles = StyleSheet.create({
+  DXxeQbwp: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  JTfPLUxu: {
+    left: 56,
+    top: 153,
+    position: "absolute",
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
---
+++ screens/Untitled6/index.js
@@ -0,0 +1,28 @@
+import { TextInput } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled6 = () => {
+  return <View style={_styles.mNRppRRg}>
+      <TextInput style={_styles.DaYWkEYY}></TextInput></View>;
+};
+
+export default Untitled6;
+
+const _styles = StyleSheet.create({
+  mNRppRRg: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  DaYWkEYY: {
+    left: 178,
+    top: 214,
+    position: "absolute",
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  }
+});
